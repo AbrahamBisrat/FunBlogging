bb668c38251f0f76731ca6982d51c0cb276d3059
Abraham Bisrat <abrahammehari0@gmail.com>
2022-03-23 01:18:49 -0500

    Added Navbar component />

---
+++ src/Tbody.js
@@ -0,0 +1,12 @@
+function Tbody(incoming) {
+  let privateVar = "Boiled Potatos";
+  let data = incoming;
+  return (
+    <div className="trial">
+      <h1>Welcome {privateVar}</h1>
+      <h2>This is my custom component</h2>
+    </div>
+  );
+}
+
+export default Tbody;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import Tbody from "./Tbody";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    {/* <Tbody /> */}
   </React.StrictMode>,
   document.getElementById("root")
 );
