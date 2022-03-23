39df587f13980026b613957fa703258b8eca5f5e
Abraham Bisrat <abrahammehari0@gmail.com>
2022-03-23 03:32:44 -0500

    Added list all blogs feature

--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,49 @@
+import { useState } from "react";
 const p = (text) => console.log(text);
-const Home = () => {
-  const handleClick = (event) => {
-    p("Click handled");
-    p(event);
-  };
-  const handleThis = (names) => p(names);
 
+const Home = () => {
+  const [blogs, setBlogs] = useState([
+    {
+      title: "Coding @ 3:20 am is fun",
+      body: "lorem ipsum....",
+      author: "bananas",
+      id: 1,
+    },
+    {
+      title: "Should you learn React?",
+      body: "lorem ipsum....",
+      author: "potatos",
+      id: 2,
+    },
+    {
+      title: "I would kill for a pizza now",
+      body: "lorem ipsum....",
+      author: "sushi",
+      id: 3,
+    },
+  ]);
+  const more = () => {
+    p(blogs);
+    setBlogs.unshift({
+      title: "new",
+      body: "ldjf",
+      author: "meaw",
+      id: 4,
+    });
+    p(blogs);
+  };
   return (
     <div className="home">
-      <h2>Homepage</h2>
-      <button onClick={(event) => handleClick(event)}>Click Me</button>
-      <button onClick={() => handleThis("potatos")}>Click me 2</button>
+      {blogs.map((post) => (
+        <div className="blog-preview" key={post.id}>
+          <h2>{post.title}</h2>
+          <p>
+            <small>Written by: {post.author}</small>
+          </p>
+          <p>{post.body}</p>
+        </div>
+      ))}
+      <button onClick={more}>Add More</button>
     </div>
   );
 };
