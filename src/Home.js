669b3ee312bcc1a4ed6d483d8c465fc8f32a4327
Abraham Bisrat <abrahammehari0@gmail.com>
2022-03-23 05:39:30 -0500

    Added list from api and delete from api endpoint

--- src/Home.js
+++ src/Home.js
@@ -6,25 +6,39 @@ const Home = () => {
   // json - server "api" endpoint
   const uri = "http://localhost:4000/blogs";
   const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
 
   const deleteHandler = (id) => {
-    setBlogs(blogs.filter((eachPost) => eachPost.id != id));
+    fetch(`${uri}/${id}`, {
+      method: "DELETE",
+    })
+      .then(() => {
+        fetchDBdata(uri);
+      })
+      .catch((err) => {
+        p("Something went wrong while deleting the blog!");
+        p(err);
+      });
+  };
+
+  const fetchDBdata = (url) => {
+    fetch(url)
+      .then((data) => data.json())
+      .then((parsedBlogArray) => {
+        setIsPending(false);
+        setBlogs(parsedBlogArray);
+      })
+      .catch((err) => p(err));
   };
   useEffect(() => {
     // run this every render of this component
     p("Use effect triggered!");
-    fetch(uri)
-      .then((data) => data.json())
-      .then((parsedBlogArray) => {
-        p("data is ready");
-        p(parsedBlogArray);
-        setBlogs(parsedBlogArray);
-      })
-      .catch((err) => p(err));
+    fetchDBdata(uri);
   }, []); // dependency for the useEffect to trigger can be adde to the array
 
   return (
     <div className="home">
+      {isPending && <h2>Loading...</h2>}
       {blogs && (
         <BlogList
           blogs={blogs}
