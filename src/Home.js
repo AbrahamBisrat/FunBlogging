1df039569d24c594979be0f566c823c3414c2435
Abraham Bisrat <abrahammehari0@gmail.com>
2022-03-23 22:59:58 -0500

    Added error message for incorrect api call

--- src/Home.js
+++ src/Home.js
@@ -4,9 +4,10 @@ const p = (text) => console.log(text);
 
 const Home = () => {
   // json - server "api" endpoint
-  const uri = "http://localhost:4000/blogs";
+  const uri = "http://localhost:4000/blogss";
   const [blogs, setBlogs] = useState(null);
   const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
   const deleteHandler = (id) => {
     fetch(`${uri}/${id}`, {
@@ -16,19 +17,26 @@ const Home = () => {
         fetchDBdata(uri);
       })
       .catch((err) => {
-        p("Something went wrong while deleting the blog!");
         p(err);
       });
   };
 
   const fetchDBdata = (url) => {
     fetch(url)
-      .then((data) => data.json())
-      .then((parsedBlogArray) => {
-        setIsPending(false);
-        setBlogs(parsedBlogArray);
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Couldn't fetch the data for that resource");
+        }
+        return res.json();
       })
-      .catch((err) => p(err));
+      .then((data) => {
+        setIsPending(false);
+        setBlogs(data);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setIsPending(false);
+      });
   };
   useEffect(() => {
     // run this every render of this component
@@ -38,6 +46,7 @@ const Home = () => {
 
   return (
     <div className="home">
+      {error && <div>{error}</div>}
       {isPending && <h2>Loading...</h2>}
       {blogs && (
         <BlogList
